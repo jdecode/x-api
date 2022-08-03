5dcf5db3a7fa77f2de8f32664b078b75278fbe1d
JD Singh <jagdeepsingh@founderandlightning.com>
2022-08-04 00:38:42 +0530

    npm install + vite server config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    "server": {
+        "host": "0.0.0.0",
+        "watch": {
+            "ignored": [
+                "!**/vendor/**"
+            ]
+        },
+        "port": 3333
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
