23a40e4deb77975772816ada4e1f1adb080661c9
JD Singh <jagdeepsingh@founderandlightning.com>
2022-08-04 23:01:27 +0530

    Changes:  - Refresh DB  - Remove pre/post build scripts  - Use Pest structure  - Rearrange some composer steps

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,21 +1,9 @@
 <?php
 
-namespace Tests\Feature;
-
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+/**
+ * Check if home page is accessible.
+ */
+it('can access home page', function () {
+    $this->get('/')
+        ->assertOk();
+});
