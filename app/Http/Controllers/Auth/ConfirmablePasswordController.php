d1030c5144ee0c0fa7bbc67a10d5bba8191421be
JD Singh <jagdeepsingh@founderandlightning.com>
2022-09-05 22:55:39 +0530

    composer run phpcbf > phpcs

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -28,10 +28,12 @@ class ConfirmablePasswordController extends Controller
      */
     public function store(Request $request)
     {
-        if (! Auth::guard('web')->validate([
+        if (
+            ! Auth::guard('web')->validate([
             'email' => $request->user()->email,
             'password' => $request->password,
-        ])) {
+            ])
+        ) {
             throw ValidationException::withMessages([
                 'password' => __('auth.password'),
             ]);
