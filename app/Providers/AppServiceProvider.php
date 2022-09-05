31458ddfdbe9f7f69bc24447253fbd2390ba8202
JD Singh <jagdeepsingh@founderandlightning.com>
2022-09-05 22:54:58 +0530

    Composer packages + scripts + hooks + breeze + telescope + IGNITION_THEME=dark + Telescope::night

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,28 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
-        //
+        if (
+            $this->app->environment('local')
+            || $this->app->environment('staging')
+            || env('ALLOW_TELESCOPE')
+        ) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
-        //
+        if ($this->app->environment('production') || $this->app->environment('staging')) {
+            URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', 'on');
+        }
     }
 }
