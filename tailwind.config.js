a6819433066a4bbaeb451534a562b323d50a06f8
JD Singh <jagdeepsingh@founderandlightning.com>
2022-09-07 01:34:58 +0530

    Add "tdl" - The Dark Light color

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                "tdl": {
+                    "50": "#FEF4F1",
+                    "100": "#FDE9E3",
+                    "200": "#FBD2C6",
+                    "300": "#F8B5A0",
+                    "400": "#F49376",
+                    "500": "#EF5D30",
+                    "600": "#ED4917",
+                    "700": "#E34212",
+                    "800": "#D03D10",
+                    "900": "#B9360E"
+                }
+            }
         },
     },
 
