4719f96146e4d54fc63257343f6a1cea97eb87d4
JD Singh <jagdeepsingh@founderandlightning.com>
2022-09-08 11:08:33 +0530

    Minor UI changes

--- resources/views/admin/loginForm.blade.php
+++ resources/views/admin/loginForm.blade.php
@@ -7,7 +7,7 @@
         </div>
 
         <div class="mx-auto text-center">
-            <span class="text-gray-500 text-2xl">Login</span>
+            <span class="text-gray-500 text-2xl">Admin Login</span>
         </div>
 
         <!-- Session Status -->
@@ -39,14 +39,14 @@
             <!-- Remember Me -->
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <input id="remember_me" type="checkbox" class="rounded border-tdl-500 text-tdl-600 shadow-sm" name="remember">
+                    <span class="ml-2 text-sm text-gray-500">{{ __('Remember me') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="hidden underline text-sm text-gray-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
