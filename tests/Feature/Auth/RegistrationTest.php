d50363ff2fc60e6f4c7b638d13aecff69ff12876
JD Singh <jagdeepsingh@founderandlightning.com>
2023-03-17 04:24:45 +0530

    PhpUnit > Pest

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,24 +1,15 @@
 <?php
 
-namespace Tests\Feature\Auth;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\post;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+test('New user can register', function () {
+    // Arrange
+    $user = createUnverifiedUser();
 
-class RegistrationTest extends TestCase
-{
-    use RefreshDatabase;
+    // Act
+    post(route('register'), $user);
 
-    public function test_new_users_can_register(): void
-    {
-        $response = $this->post(route('register'), [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
-        ]);
-
-        $this->assertAuthenticated();
-        $response->assertNoContent();
-    }
-}
+    // Assert
+    assertDatabaseHas('users', ['email' => $user['email']]);
+})->group('api', 'register');
