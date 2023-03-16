4545b35ec99dc685c29af6580a5fba7a9adf5dab
JD Singh <jagdeepsingh@founderandlightning.com>
2023-03-17 04:22:38 +0530

    Remove comments

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -19,11 +19,7 @@ class LoginRequest extends FormRequest
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-     */
+
     public function rules(): array
     {
         return [
