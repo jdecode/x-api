fc2fe7f42416cdbffa175431d3f9698dd47c8361
JD Singh <jagdeepsingh@founderandlightning.com>
2023-03-17 04:17:09 +0530

    Selectively remove from auth file

--- routes/auth.php
+++ routes/auth.php
@@ -3,16 +3,8 @@
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-                ->middleware('guest')
-                ->name('login');
-
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->middleware('guest')
-                ->name('password.email');
 
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->middleware('guest')
