3e0b568e4cc71693f7c33f0a2442362f759d1d73
JD Singh <jagdeepsingh@founderandlightning.com>
2023-03-18 03:50:10 +0530

    Convert to Pest

--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -1,47 +1,36 @@
 <?php
 
-namespace Tests\Feature\Auth;
-
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Notification;
-use Tests\TestCase;
 
-class PasswordResetTest extends TestCase
+test('reset password link can be requested', function ()
 {
-    use RefreshDatabase;
+    Notification::fake();
 
-    public function test_reset_password_link_can_be_requested(): void
-    {
-        Notification::fake();
+    $user = User::factory()->create();
 
-        $user = User::factory()->create();
+    $this->post(route('password.email'), ['email' => $user->email]);
 
-        $this->post(route('password.email'), ['email' => $user->email]);
+    Notification::assertSentTo($user, ResetPassword::class);
+});
 
-        Notification::assertSentTo($user, ResetPassword::class);
-    }
+test('password can be reset with valid token', function () {
+    Notification::fake();
 
-    public function test_password_can_be_reset_with_valid_token(): void
-    {
-        Notification::fake();
+    $user = User::factory()->create();
 
-        $user = User::factory()->create();
+    $this->post(route('password.email'), ['email' => $user->email]);
 
-        $this->post(route('password.email'), ['email' => $user->email]);
+    Notification::assertSentTo($user, ResetPassword::class, function (object $notification) use ($user) {
+        $response = $this->post('/reset-password', [
+            'token' => $notification->token,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
 
-        Notification::assertSentTo($user, ResetPassword::class, function (object $notification) use ($user) {
-            $response = $this->post('/reset-password', [
-                'token' => $notification->token,
-                'email' => $user->email,
-                'password' => 'password',
-                'password_confirmation' => 'password',
-            ]);
+        $response->assertSessionHasNoErrors();
 
-            $response->assertSessionHasNoErrors();
-
-            return true;
-        });
-    }
-}
+        return true;
+    });
+})->group('auth', 'password-reset');
